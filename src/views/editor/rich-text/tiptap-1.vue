c89ea66e3d0dbf69814e83542cd664b5b64ee336
lotusloli <nymphaeal@qq.com>
2024-05-23 22:43:52 +0800

    1. tiptap

---
+++ src/views/editor/rich-text/tiptap-1.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { onMounted } from 'vue'
+
+const editor = new Editor({
+  content: 'talk is cheap',
+  extensions: [StarterKit],
+})
+
+onMounted(() => {})
+</script>
+
+<template>
+  <div class="p-2">
+    <editor-content :editor="editor"></editor-content>
+  </div>
+</template>
+
+<style lang="scss"></style>
